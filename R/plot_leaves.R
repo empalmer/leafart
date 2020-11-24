ac7b8c81e4506662d4306d7b0e82dcdcbf1c37ca
Emily Palmer <palmerem@oregonstate.edu>
2020-11-24 14:22:15 -0800

    plot function plots single leaf as tree #12

--- R/plot_leaves.R
+++ R/plot_leaves.R
@@ -2,16 +2,24 @@
 plot_leaves <- function(leaves,
                         pal = c( "#D1CEC5", "#997C67", "#755330",
                                  "#B0703C", "#DBA72E", "#E3CCA1")){
-
-
-  p <- ggplot(leaves,aes(x = x,y= y,
-                         #group= leaf_id,
-                         color = factor(color))) +
-    geom_path() +
-    theme_void() +
-    theme(legend.position = "none",
-          plot.background = element_rect(fill = "gray74")) +
-    scale_color_manual(values = pal)
+  # Are we plotting multiple leaves?
+  if("leaf_id" %in% colnames(leaves)){
+    p <- ggplot(leaves,aes(x = x,y= y,
+                           #group= leaf_id,
+                           color = factor(color))) +
+      geom_path() +
+      theme_void() +
+      theme(legend.position = "none",
+            plot.background = element_rect(fill = "gray74")) +
+      scale_color_manual(values = pal)
+  }
+  # Plot only one leaf (no color)
+  else{
+    p <- ggplot(leaves,aes(x = x, y= y)) +
+      geom_path() +
+      theme_void() +
+      theme(legend.position = "none",
+            plot.background = element_rect(fill = "gray74"))
+  }
   return(p)
-
 }
